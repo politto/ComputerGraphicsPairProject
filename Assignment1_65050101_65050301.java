06741e9b2107ba1912ea987c1ac4a152a5620d10
Nattaphat <eakacit@gmail.com>
2023-12-23 14:45:09 +0700

    MockupProfile and get return updated

--- Assignment1_65050101_65050301.java
+++ Assignment1_65050101_65050301.java
@@ -10,7 +10,7 @@ public class Assignment1_65050101_65050301 extends JPanel{
 
         JFrame jf = new JFrame();
         jf.add(wr);
-        jf.setTitle("Project_Of_65050301_65050xxx");
+        jf.setTitle("Project_Of_65050301_65050101");
         jf.setSize(600, 600);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
@@ -22,20 +22,90 @@ public class Assignment1_65050101_65050301 extends JPanel{
         BufferedImage buffer = new BufferedImage(601, 601, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = buffer.createGraphics();
 
-        g2.setColor(Color.WHITE);
+        g2.setColor(new Color(200, 152, 87));
         g2.fillRect(0, 0, 600, 600);
 
         g2.setColor(Color.BLACK);
         // bezierCurve(g2, 100, 100, 500, 500, 100, 500, 500, 100);
         // BresenhamLine(g2, 100, 100, 500, 100);
 
-        int[] xpoly = {100, 250, 550, 400, 250, 90 ,40};
-        int[] ypoly = {100, 50, 150, 300, 500, 400, 200};
-        Polygon poly = new Polygon(xpoly, ypoly,7);
-        g2.drawPolygon(poly);
-        buffer = floodFill(buffer, 480, 110, Color.WHITE, Color.BLUE);
+        // int[] xpoly = {100, 250, 550, 400, 250, 90 ,40};
+        // int[] ypoly = {100, 50, 150, 300, 500, 400, 200};
+        // Polygon poly = new Polygon(xpoly, ypoly,7);
+        // g2.drawPolygon(poly);
+        // buffer = floodFill(buffer, 480, 110, new Color(200, 152, 87), Color.BLUE);
+        paintHead(g2);
+        paintSep(g2);
+
+
         g.drawImage(buffer, 0, 0, null);
         
+    }
+
+    void paintHead(Graphics g) {
+        // bezierCurve(g
+        // , 100, 220
+        // , 102, 220
+        // , 104, 222
+        // , 104, 224);
+        // bezierCurve(g
+        // , 100, 220
+        // , 98, 220
+        // , 96, 222
+        // , 96, 224);
+        // bezierCurve(g
+        // , 96, 224
+        // , 96, 228
+        // , 100, 224
+        // , 100, 228);
+        // bezierCurve(g
+        // , 100, 228
+        // , 104, 228
+        // , 100, 224
+        // , 104, 224);
+        drawCircle(g, 102, 220, 10);
+
+        
+    }
+    void drawCircle(Graphics g, int cenx, int ceny, int rad){
+        if (rad % 2 > 0){
+            System.out.println("Error in this case rad must be even!");
+            return;
+        } 
+        bezierCurve(g, cenx - rad, ceny, cenx - rad, ceny - rad / 2, cenx - rad / 2, ceny - rad, cenx, ceny - rad);
+        bezierCurve(g, cenx, ceny - rad, cenx + rad / 2, ceny - rad, cenx + rad, ceny - rad / 2, cenx + rad, ceny);
+        bezierCurve(g, cenx + rad, ceny, cenx + rad, ceny + rad / 2, cenx + rad / 2, ceny + rad, cenx, ceny + rad);
+        
+        
+    }
+
+    void paintSep(Graphics g2) {
+        bresenhamLine(g2, 300, 0, 300, 120);
+        bresenhamLine(g2, 300, 120, 310, 110);
+        bresenhamLine(g2, 310, 110, 360, 80);
+        bresenhamLine(g2, 360, 80, 380, 75);
+        bresenhamLine(g2, 380, 75, 370, 90);
+        bresenhamLine(g2, 370, 90, 410, 80);
+        bresenhamLine(g2, 410, 80, 395, 110);
+        bresenhamLine(g2, 395, 110, 390, 130);
+        bresenhamLine(g2, 390, 130, 395, 135);
+        bresenhamLine(g2, 395, 135, 380, 150);
+        bresenhamLine(g2, 380, 150, 375, 200);
+        bresenhamLine(g2, 375, 200, 380, 220);
+        bresenhamLine(g2, 380, 220, 360, 218);
+        bresenhamLine(g2, 360, 218, 320, 260);
+        bresenhamLine(g2, 320, 260, 280, 350);
+        bresenhamLine(g2, 280, 350, 240, 360);
+        bresenhamLine(g2, 240, 360, 275, 380);
+        bresenhamLine(g2, 275, 380, 290, 400);
+        bresenhamLine(g2, 290, 400, 290, 430);
+        bresenhamLine(g2, 290, 430, 270, 450);
+        bresenhamLine(g2, 270, 450, 230, 500);
+        bresenhamLine(g2, 230, 500, 210, 505);
+        bresenhamLine(g2, 210, 505, 220, 512);
+        bresenhamLine(g2, 220, 512, 205, 520);
+        bresenhamLine(g2, 205, 520, 210, 528);
+        bresenhamLine(g2, 210, 528, 210, 600);
     }
 
     void bezierCurve(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){
@@ -120,7 +190,7 @@ public class Assignment1_65050101_65050301 extends JPanel{
         return m;
     }
 
-    void BresenhamLine(Graphics g, int x1, int y1, int x2, int y2){
+    void bresenhamLine(Graphics g, int x1, int y1, int x2, int y2){
         float dx = Math.abs(x2 - x1);
         float dy = Math.abs(y2 - y1);
 
